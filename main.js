63a57891a62102a21a7795d45f45546fb67a41e5
kairi Yasumatsu <kairi.matibuse@gmail.com>
2020-02-01 16:48:03 +0900

    編集を追加

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var app = new Vue({
   el: '#app',
   data: {
+    edittext: "",
+    editkey: "",
     text: "",
+    edit: false,
     todos: JSON.parse(localStorage.list || "[]")
   },
   methods: {
@@ -13,6 +16,16 @@ var app = new Vue({
     removeTodo(key) {
       this.todos.splice(key, 1);
       this.saveTodo()
+    },
+    editshow(key){
+      this.edit = true
+      this.editkey = (key)
+    },
+    editvalue(){
+      this.todos[this.editkey] = this.edittext
+     this.saveTodo()
+     this.edittext = ""
+     this.edit = false
     },
     saveTodo() {
       const jsonText = JSON.stringify(this.todos)
